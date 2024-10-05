fe5c9b88762e3ef0bdf25a3230224e7df2c76800
valtria@MSI <valtria@MSI>
2024-10-05 22:47:06 +0200

    finalisation de la grille de caracter + nouveaux fichier

--- OCR/solver.c
+++ OCR/solver.c
@@ -22,18 +22,64 @@ void to_uppercase(char *str)
 
 }
 
-size_t my_strlen(const char str[])
+int my_strlen(char str[])
 {
-       size_t conteur=0;
        int i = 0;
 
        while(str[i] != '\0')
        {
-             conteur = conteur + 1;
              i = i + 1;
        }
 
-       return conteur;
+       return i;
+}
+
+void print_two_dim(int row, int col, char arr[row][col])
+{
+   if(row == 0 && col == 0)
+   {
+      printf("[]\n");
+   }
+
+   else
+   {
+      printf("[\n");
+
+      for(int i = 0; i < row; i++)
+      {  
+         printf(" [");
+
+         for(int j = 0; j < col - 1; j++)
+         {
+            printf("%i, ",arr[i][j]);
+         }
+         
+            printf("%i]\n", arr[i][col-1]);
+      }
+
+      printf("]\n" );
+   } 
+}
+
+void print_one_dim(char arr[], int size)
+{
+
+   if(size == 0)
+   {
+      printf("[]\n");
+   }
+
+   else
+   {
+      printf("[");
+
+      for(int i = 0; i < size - 1; i++)
+      {
+          printf("%c, ",arr[i]);
+      }
+   
+      printf("%d]\n", arr[size-1]);  
+   } 
 }
 
 
@@ -52,7 +98,7 @@ int main(int argc, char *argv[])
 
      to_uppercase(argv[0]);
      
-     //char grid[2][2];
+     int len = my_strlen(argv[2]); //length of the world to be search
 
      FILE *fp;
 
@@ -63,23 +109,38 @@ int main(int argc, char *argv[])
 	     printf("Error: not such file");
 	     return 1;
      }
-     
-     char str[1000];
-     int i, j = 0;
-
-     while(fgets(str, 1000, fp)!= NULL)
-     {
-	     printf("%s",str);
-	     i++;
-             j++;	     
-     }
-
-    printf("%i,%i\n",i,j);
-    fclose(fp); 
+        
     
+    char grid[9][10];
+    
+    
+    int i = 0;
+    int j = 0;
+    char charac;
+    while ((charac = fgetc(fp)) != EOF)
+    {
+	if (charac == '\n')
+        {
+                j = 0;
+                i++;
+        }
 
-    printf("Not found\n");
-    return 0;
+	else if (charac >= 'A' && charac<='Z')
+        {
+                grid[i][j] = charac;
+                j++;
+        }
+    }
+
+     fclose(fp);
+
+
+
+     print_two_dim(9,10,grid);
+     printf("%i,%i\n",i,j);
+
+     printf("Not found\n");
+     return 0;
 
      	
 }
---
+++ OCR/xor.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+int main()
+{
+    return 0;
+}
