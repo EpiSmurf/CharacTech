385b8d74a7060290e346f5f7772866287c896f7e
Guillaume <guillaume.dufour@epita.fr>
2024-11-02 12:58:41 +0100

    qidhqiudhq

--- CharacTech/Formatage/grid.c
+++ CharacTech/Formatage/grid.c
@@ -4,141 +4,154 @@
 #include <SDL2/SDL_image.h>
 #include <err.h>
 
-void fix_contrast(Uint8 * r, Uint8 * g, Uint8 *b, int min_int, int max_int)
+// Adjusts the contrast of a pixel's RGB values based on minimum and maximum intensities
+void fix_contrast(Uint8 *r, Uint8 *g, Uint8 *b, int min_int, int max_int)
 {
-    
     int temp = 0;
-    temp = ((*r - min_int)*(255 / (max_int-min_int)));
-    if(temp > 255)
-        temp =255;
-    if(temp < 0)
-        temp =0;
+    
+    // Adjust red channel
+    temp = ((*r - min_int) * (255 / (max_int - min_int)));
+    if (temp > 255) temp = 255;
+    if (temp < 0) temp = 0;
     *r = temp;
-    temp = ((*g - min_int)*(255 / (max_int-min_int)));
-    if(temp > 255)
-        temp =255;
-    if(temp < 0)
-        temp =0;
+
+    // Adjust green channel
+    temp = ((*g - min_int) * (255 / (max_int - min_int)));
+    if (temp > 255) temp = 255;
+    if (temp < 0) temp = 0;
     *g = temp;
-    temp = ((*b - min_int)*(255 / (max_int-min_int)));
-    if(temp > 255)
-        temp =255;
-    if(temp < 0)
-        temp =0;
+
+    // Adjust blue channel
+    temp = ((*b - min_int) * (255 / (max_int - min_int)));
+    if (temp > 255) temp = 255;
+    if (temp < 0) temp = 0;
     *b = temp;
 }
 
+// Converts an SDL surface to grayscale
 void grayscale(SDL_Surface *surface)
 {
     Uint32 *pix = (Uint32 *)surface->pixels;
     SDL_LockSurface(surface);
     SDL_PixelFormat *format = surface->format;
+
     int min_int = 255;
     int max_int = 0;
-    int intencity = 0;
+    int intensity = 0;
     int gray = 0;
-    for(int i = 0; i<surface->w; i++)
+
+    // Convert each pixel to grayscale and calculate intensity range
+    for (int i = 0; i < surface->w; i++)
     {
-        for(int j =0; j<surface->h;j++)
+        for (int j = 0; j < surface->h; j++)
         {
-            Uint32 p = pix[ i + (j * surface->w)];
-            Uint8 r = 0;
-            Uint8 g = 0;
-            Uint8 b = 0;
-            SDL_GetRGB(p,format,&r,&g,&b);
-            r = (r*30)/100;
-            g = (g*59)/100;
-            b = (b*11)/100;
-            gray = r+g+b;
-            intencity = (r+g+b)/3;
-            if(intencity > max_int)
-                max_int = intencity;
-            if (intencity < min_int)
-                min_int = intencity;
-            p = SDL_MapRGB(format,gray,gray,gray);
+            Uint32 p = pix[i + (j * surface->w)];
+            Uint8 r = 0, g = 0, b = 0;
+
+            SDL_GetRGB(p, format, &r, &g, &b);
+            r = (r * 30) / 100;
+            g = (g * 59) / 100;
+            b = (b * 11) / 100;
+            gray = r + g + b;
+
+            intensity = (r + g + b) / 3;
+            if (intensity > max_int) max_int = intensity;
+            if (intensity < min_int) min_int = intensity;
+
+            p = SDL_MapRGB(format, gray, gray, gray);
             pix[i + (j * surface->w)] = p;
         }
     }
-    /*for(int i = 0; i<surface->w; i++)
+
+    // Uncomment this section to adjust contrast after grayscale conversion
+    /*
+    for (int i = 0; i < surface->w; i++)
     {
-        for(int j =0; j<surface->h;j++)
+        for (int j = 0; j < surface->h; j++)
         {
-            Uint32 p = pix[ i + (j * surface->w)];
-            Uint8 r = 0;
-            Uint8 g = 0;
-            Uint8 b = 0;
-            SDL_GetRGB(p,format,&r,&g,&b);
-            fix_contrast(&r,&g,&b,min_int,max_int);
-            p = SDL_MapRGB(format,r,g,b);
+            Uint32 p = pix[i + (j * surface->w)];
+            Uint8 r = 0, g = 0, b = 0;
+
+            SDL_GetRGB(p, format, &r, &g, &b);
+            fix_contrast(&r, &g, &b, min_int, max_int);
+            p = SDL_MapRGB(format, r, g, b);
             pix[i + (j * surface->w)] = p;
         }
-    }*/
+    }
+    */
+
     SDL_UnlockSurface(surface);
 }
 
 int main(int argc, char** argv)
 {
-    printf("Image to load : '%s'\n",argv[1]);
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    if(!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
-        errx(EXIT_FAILURE, "Failed to initialise SDL_image: %s", SDL_GetError());
+    printf("Image to load: '%s'\n", argv[1]);
 
+    // Initialize SDL and SDL_image
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "SDL initialization error: %s", SDL_GetError());
+    if (!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
+        errx(EXIT_FAILURE, "Failed to initialize SDL_image: %s", SDL_GetError());
 
+    // Check for correct argument count
     if (argc != 2)
-       errx(EXIT_FAILURE, "Usage: image-file");
-    if(SDL_Init(SDL_INIT_VIDEO)!=0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+        errx(EXIT_FAILURE, "Usage: %s <image-file>", argv[0]);
 
-    SDL_Window* window = SDL_CreateWindow("Window",0, 0, 0, 0,
-		    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if(window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    // Create SDL window
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (!window)
+        errx(EXIT_FAILURE, "Window creation error: %s", SDL_GetError());
+
+    // Create SDL renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    if (!renderer)
+        errx(EXIT_FAILURE, "Renderer creation error: %s", SDL_GetError());
 
-    printf("Before load");
+    // Load image file into a surface
     SDL_Surface* t = IMG_Load(argv[1]);
-    if (t == NULL)
-        {
-            printf("Fail load");
-            errx(EXIT_FAILURE, "%s", SDL_GetError());
-        }
-    printf("After load");
+    if (!t)
+        errx(EXIT_FAILURE, "Image load error: %s", SDL_GetError());
+
+    // Convert surface to required format
     SDL_Surface *surface = SDL_ConvertSurfaceFormat(t, SDL_PIXELFORMAT_RGB888, 0);
     SDL_FreeSurface(t);
-    if (surface == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    SDL_SetWindowSize(window,  surface->w, surface->h);
+    if (!surface)
+        errx(EXIT_FAILURE, "Surface conversion error: %s", SDL_GetError());
 
+    SDL_SetWindowSize(window, surface->w, surface->h);
+
+    // Apply grayscale filter to the surface
     grayscale(surface);
+
+    // Create texture from the modified surface
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    SDL_RenderCopy(renderer,texture,NULL,NULL);
+    // Render texture to window
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
+
     SDL_Event event;
     while (1)
     {
         SDL_WaitEvent(&event);
-        switch (event.type)
+        if (event.type == SDL_QUIT)
         {
-            case SDL_QUIT:
-                SDL_FreeSurface(surface);
-                SDL_DestroyTexture(texture);
-                SDL_DestroyRenderer(renderer);
-                SDL_DestroyWindow(window);
-                SDL_Quit();
-                return EXIT_SUCCESS;
-
-            case SDL_WINDOWEVENT:
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-                {
-                        SDL_RenderCopy(renderer,texture,NULL,NULL);
-                        SDL_RenderPresent(renderer);
-                }
-                break;
+            // Cleanup and exit on quit event
+            SDL_FreeSurface(surface);
+            SDL_DestroyTexture(texture);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            IMG_Quit();
+            SDL_Quit();
+            return EXIT_SUCCESS;
+        }
+        else if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED)
+        {
+            // Re-render on window resize
+            SDL_RenderCopy(renderer, texture, NULL, NULL);
+            SDL_RenderPresent(renderer);
         }
     }
+
     return EXIT_SUCCESS;
 }
--- CharacTech/Formatage/turn.c
+++ CharacTech/Formatage/turn.c
@@ -1,6 +1,4 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,67 +6,90 @@
 
 int main(int argc, char** argv)
 {
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    if(!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
-        errx(EXIT_FAILURE, "Failed to initialise SDL_image: %s", SDL_GetError());
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "SDL initialization error: %s", SDL_GetError());
 
+    // Initialize SDL_image with JPG support
+    if (!(IMG_Init(IMG_INIT_JPG) & IMG_INIT_JPG))
+        errx(EXIT_FAILURE, "Failed to initialize SDL_image with JPG support: %s", IMG_GetError());
 
+    // Check for correct argument count
     if (argc != 3)
-       errx(EXIT_FAILURE, "Wrong amount of arguments");
-    if(SDL_Init(SDL_INIT_VIDEO)!=0)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+        errx(EXIT_FAILURE, "Usage: %s <image_path> <angle>", argv[0]);
 
-    SDL_Window* window = SDL_CreateWindow("Window",0, 0, 0, 0,
-		    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if(window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    // Create SDL window
+    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (!window)
+        errx(EXIT_FAILURE, "Window creation error: %s", SDL_GetError());
 
-    SDL_Surface* t = IMG_Load(argv[1]);
-    if (t == NULL)
-        {
-            errx(EXIT_FAILURE, "%s", SDL_GetError());
-        }
-    SDL_Surface *surface = SDL_ConvertSurfaceFormat(t, SDL_PIXELFORMAT_RGB888, 0);
-    SDL_FreeSurface(t);
-    if (surface == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-    SDL_SetWindowSize(window,  surface->w, surface->h);
+    // Create SDL renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer)
+        errx(EXIT_FAILURE, "Renderer creation error: %s", SDL_GetError());
+
+    // Load image into a surface
+    SDL_Surface* temp_surface = IMG_Load(argv[1]);
+    if (!temp_surface)
+        errx(EXIT_FAILURE, "Image loading error: %s", IMG_GetError());
+
+    // Convert surface to target format
+    SDL_Surface* surface = SDL_ConvertSurfaceFormat(temp_surface, SDL_PIXELFORMAT_RGB888, 0);
+    SDL_FreeSurface(temp_surface);
+    if (!surface)
+        errx(EXIT_FAILURE, "Surface conversion error: %s", SDL_GetError());
+
+    // Set window size to match the image
+    SDL_SetWindowSize(window, surface->w, surface->h);
+
+    // Create a texture from the surface
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_RenderCopy(renderer,texture,NULL,NULL);
+    SDL_FreeSurface(surface);  // Free surface after texture creation
+    if (!texture)
+        errx(EXIT_FAILURE, "Texture creation error: %s", SDL_GetError());
 
-    char * c;
-    const double angle = strtod(argv[2],&c);
-    SDL_Point pivot = {surface->w/2,surface->h/2};
-    SDL_Rect DRect = {0,0,surface->w,surface->h};
-    SDL_RenderCopyEx(renderer,texture,NULL,&DRect,angle,&pivot,SDL_FLIP_NONE);
+    // Parse rotation angle from arguments
+    char* end;
+    double angle = strtod(argv[2], &end);
+    if (*end != '\0')
+        errx(EXIT_FAILURE, "Invalid angle: %s", argv[2]);
 
+    // Set rotation pivot to the center of the image
+    SDL_Point pivot = { surface->w / 2, surface->h / 2 };
+    SDL_Rect dest_rect = { 0, 0, surface->w, surface->h };
+
+    // Render the image with rotation
+    SDL_RenderClear(renderer);
+    SDL_RenderCopyEx(renderer, texture, NULL, &dest_rect, angle, &pivot, SDL_FLIP_NONE);
     SDL_RenderPresent(renderer);
-    SDL_Event event;
-    while (1)
-    {
-        SDL_WaitEvent(&event);
-        switch (event.type)
-        {
-            case SDL_QUIT:
-                SDL_FreeSurface(surface);
-                SDL_DestroyTexture(texture);
-                SDL_DestroyRenderer(renderer);
-                SDL_DestroyWindow(window);
-                SDL_Quit();
-                return EXIT_SUCCESS;
 
-            case SDL_WINDOWEVENT:
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED)
-                {
-                        SDL_RenderCopy(renderer,texture,NULL,NULL);
-                        SDL_RenderPresent(renderer);
-                }
-                break;
+    // Main event loop
+    SDL_Event event;
+    int running = 1;
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = 0;
+            }
+            else if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED)
+            {
+                // Re-render image on resize
+                SDL_RenderClear(renderer);
+                SDL_RenderCopyEx(renderer, texture, NULL, &dest_rect, angle, &pivot, SDL_FLIP_NONE);
+                SDL_RenderPresent(renderer);
+            }
         }
     }
+
+    // Cleanup resources
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
